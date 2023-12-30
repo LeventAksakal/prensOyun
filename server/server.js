e9e1b455b5f064a4d48fa51659c6f5b4d08ac7b9
KeremBTR <94068893+KeremBTR@users.noreply.github.com>
2023-12-30 22:36:00 +0300

    Matchmake is almost done...

--- server/server.js
+++ server/server.js
@@ -68,23 +68,107 @@ io.use((socket, next) => {
 });
 
 const activePlayers = {};
+const queueOfPong = [];
+const queueOfBattleship = [];
 
 io.on("connection", (socket) => {
-  activePlayers[socket.userId] = socket.userId;
+  // for debug
+  console.log(`${socket.id} connected`);
+
+  activePlayers[socket.id] = socket.userId;
   io.emit("active-players", activePlayers);
+
   socket.on("disconnect", () => {
-    delete activePlayers[socket.userId];
+    // for debug
+    console.log(`${socket.id} disconnected`);
+    delete activePlayers[socket.id];
+
+    const indexInQueuePong = queueOfPong.findIndex(
+      (item) => item.id === socket.id
+    );
+
+    if (indexInQueuePong !== -1) {
+      queueOfPong.splice(indexInQueuePong, 1);
+    }
+
     io.emit("active-players", activePlayers);
   });
+
   socket.on("ping", (time) => {
     socket.emit("pong", time);
   });
+
   socket.on("pong-request", ({ nickname }) => {
+    // for debug
+    console.log(
+      `${socket.id} requested pong with nickname: ${nickname || "Anonymous"}`
+    );
+
     if (!nickname) {
       nickname = "Anonymous";
     }
-    const gameId = uuidv4();
-    io.emit("redirect", gameId);
+
+    const isInQueueP = queueOfPong.some((item) => item.id === socket.id);
+    const isInQueueBS = queueOfBattleship.some((item) => item.id === socket.id);
+
+    if (!isInQueueP && !isInQueueBS) {
+      queueOfPong.push(socket);
+
+      if (queueOfPong.length >= 2) {
+        const player1 = queueOfPong.shift();
+        const player2 = queueOfPong.shift();
+
+        const roomId = uuidv4();
+        // matched
+        player1.join(roomId);
+        player2.join(roomId);
+        // for debug
+        console.log(`${socket.id} joined room ${roomId}`);
+        
+        io.to(roomId).emit("redirect", roomId);
+        // for debug
+        console.log(
+          `Players matched. Room ID: ${roomId}, Player 1: ${player1}, Player 2: ${player2}`
+        );
+      } else {
+        io.to(socket.id).emit("waiting-in-queue");
+        // for debug
+        console.log(`${socket.id} is waiting for matching...`);
+      }
+    } else {
+      io.to(socket.id).emit("already-in-queue");
+      console.log("Already in the queue...");
+    }
+  });
+
+  socket.on("battleship-request", ({ nickname }) => {
+    if (!nickname) {
+      nickname = "Anonymous";
+    }
+
+    const isInQueueP = queueOfPong.some((item) => item.id === socket.id);
+    const isInQueueBS = queueOfBattleship.some((item) => item.id === socket.id);
+
+    if (!isInQueueP && !isInQueueBS) {
+      queueOfBattleship.push(socket);
+
+      if (queueOfBattleship.length >= 2) {
+        const player1 = queueOfBattleship.shift();
+        const player2 = queueOfBattleship.shift();
+
+        const roomId = uuidv4();
+
+        player1.join(roomId);
+        player2.join(roomId);
+
+        io.to(player1.id).emit("redirect", roomId);
+        io.to(player2.id).emit("redirect", roomId);
+      } else {
+        io.to(socket.id).emit("waiting-in-queue");
+      }
+    } else {
+      io.to(socket.io).emit("already-in-queue");
+    }
   });
 });
 
